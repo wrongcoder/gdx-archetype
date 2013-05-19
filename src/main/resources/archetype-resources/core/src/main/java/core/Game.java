404ba23b7a72f54000dc23e9a0e1b924aae3ace8
wrongcoder <git@wrongcoder.com>
2013-05-18 21:40:39 -0700

    Make version properties more accessible

--- src/main/resources/archetype-resources/core/src/main/java/core/Game.java
+++ src/main/resources/archetype-resources/core/src/main/java/core/Game.java
@@ -6,9 +6,14 @@ package ${package}.core;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+import com.badlogic.gdx.utils.SerializationException;
 
 public class Game extends com.badlogic.gdx.Game {
 	private static final Game instance = new Game();
+	private String version = "";
 	private Runnable postInit = new EmptyRunnable();
 
 	public static final int WIDTH = 800;
@@ -23,6 +28,7 @@ public class Game extends com.badlogic.gdx.Game {
 	public void create() {
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 		assetManager = new AssetManager();
+		version = loadVersion();
 		setScreen(new LoadingScreen(this));
 		postInit.run();
 	}
@@ -35,6 +41,10 @@ public class Game extends com.badlogic.gdx.Game {
 		return assetManager;
 	}
 
+	public String getVersion() {
+		return version;
+	}
+
 	public void setPostInit(final Runnable postInit) {
 		this.postInit = postInit;
 	}
@@ -42,6 +52,37 @@ public class Game extends com.badlogic.gdx.Game {
 	private static class EmptyRunnable implements Runnable {
 		@Override
 		public void run() {
+		}
+	}
+
+	private static String loadVersion() {
+		try {
+			final FileHandle versionFile = Gdx.files.internal("version.json");
+			final JsonValue versionRoot = new JsonReader().parse(versionFile);
+			final String versionNumber = versionRoot.get("version").asString();
+			final String buildNumber = versionRoot.get("buildNumber").asString();
+			final String buildDate = versionRoot.get("buildDate").asString();
+			final String commit = versionRoot.get("commit").asString();
+
+			String version;
+
+			if (buildNumber != null && !buildNumber.isEmpty() && !buildNumber.contains("${symbol_dollar}{")) {
+				version = "Build " + buildNumber;
+			} else if (commit != null && !commit.isEmpty() && !commit.contains("${symbol_dollar}{")) {
+				version = "Test build " + commit;
+			} else {
+				version = "Version " + versionNumber;
+			}
+
+			if (buildDate != null && !buildDate.isEmpty() && !buildDate.contains("${symbol_dollar}{")) {
+				version += " (" + buildDate + ")";
+			}
+
+			return version;
+
+		} catch (SerializationException e) {
+			Gdx.app.log("${projectTitle}", "Exception", e);
+			return "(unknown version)";
 		}
 	}
 
