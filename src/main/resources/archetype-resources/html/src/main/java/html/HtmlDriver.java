eeda2c904e4191db092bb5fd4d2a9e9b4721f5f6
wrongcoder <git@wrongcoder.com>
2013-05-20 13:25:02 -0700

    Centre game canvas on screen

--- src/main/resources/archetype-resources/html/src/main/java/html/HtmlDriver.java
+++ src/main/resources/archetype-resources/html/src/main/java/html/HtmlDriver.java
@@ -6,8 +6,8 @@ package ${package}.html;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.Element;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
 import ${package}.core.Game;
 
 public class HtmlDriver extends GwtApplication {
@@ -15,7 +15,7 @@ public class HtmlDriver extends GwtApplication {
 	@Override
 	public void onModuleLoad() {
 		super.onModuleLoad();
-		Game.instance().setPostInit(new RemoveLoadingMessage());
+		Game.instance().setPostInit(new WebPageSetup());
 	}
 
 	@Override
@@ -28,12 +28,33 @@ public class HtmlDriver extends GwtApplication {
 		return new GwtApplicationConfiguration(Game.WIDTH, Game.HEIGHT);
 	}
 
-	private static class RemoveLoadingMessage implements Runnable {
+	private static class WebPageSetup implements Runnable {
 		@Override
 		public void run() {
-			final Element loadingElement = DOM.getElementById("loading");
+			removeLoadingMessage();
+			centreGameCanvas();
+		}
+
+		private static void removeLoadingMessage() {
+			final Element loadingElement = Document.get().getElementById("loading");
 			if (loadingElement != null) {
 				loadingElement.removeFromParent();
+			}
+		}
+
+		private static void centreGameCanvas() {
+			final Element tableElement = Document.get().getElementsByTagName("table").getItem(0);
+			if (tableElement != null) {
+				final String style = tableElement.getAttribute("style")
+						+ " position: absolute;"
+						+ " top: 50%; margin-top: -" + Game.HEIGHT / 2 + "px;"
+						+ " left: 50%; margin-left: -" + Game.WIDTH / 2 + "px;";
+				tableElement.setAttribute("style", style);
+			}
+			final Element bodyElement = Document.get().getElementsByTagName("body").getItem(0);
+			if (bodyElement != null) {
+				bodyElement.removeAttribute("style");
+				bodyElement.addClassName("loaded");
 			}
 		}
 	}
