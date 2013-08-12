cae32dfe5df9c78898cd603998b077d8519aad8a
wrongcoder <git@wrongcoder.com>
2013-08-11 18:39:13 -0700

    Make desktop target log to disk

--- src/main/resources/archetype-resources/desktop/src/main/java/desktop/DesktopDriver.java
+++ src/main/resources/archetype-resources/desktop/src/main/java/desktop/DesktopDriver.java
@@ -7,9 +7,38 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ${package}.core.Registry;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Date;
+
 public class DesktopDriver {
 
-	public static void main(final String[] args) throws Exception {
+	public static void main(final String[] args) {
+		try {
+			final FileOutputStream logOutputStream = new FileOutputStream("${artifactId}.log", true);
+			final PrintStream logStream = new PrintStream(logOutputStream);
+			System.setOut(logStream);
+			System.setErr(logStream);
+		} catch (final FileNotFoundException e) {
+			System.err.println("Unable to open log file");
+			e.printStackTrace(System.err);
+		}
+
+		System.out.println();
+		System.out.println();
+		System.out.println("**********");
+		System.out.println("Launched at " + new Date());
+
+		try {
+			start();
+		} catch (final Throwable t) {
+			System.err.println("Caught an uncaught exception at " + new Date());
+			t.printStackTrace(System.err);
+		}
+	}
+
+	private static void start() {
 		final Registry r = new Registry();
 
 		final LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
