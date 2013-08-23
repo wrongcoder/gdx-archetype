35f7edb903221aceb504a887a478ca9b376c5743
wrongcoder <git@wrongcoder.com>
2013-08-22 20:46:22 -0700

    Add unique client ID generator
    This does nothing unless explicitly invoked.

--- src/main/resources/archetype-resources/core/src/main/java/core/Registry.java
+++ src/main/resources/archetype-resources/core/src/main/java/core/Registry.java
@@ -3,6 +3,8 @@
 #set( $symbol_escape = '\' )
 package ${package}.core;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.utils.Logger;
 
 /** Registry and initializer of global resources */
@@ -44,6 +46,32 @@ public class Registry {
 		if (platformInitializer != null) {
 			platformInitializer.run();
 		}
+	}
+
+	public String getClientId() {
+		final Preferences preferences = Gdx.app.getPreferences("${projectTitle}");
+
+		String clientId = preferences.getString("clientId", "");
+		if (clientId.isEmpty()) {
+			clientId = generateClientId();
+			preferences.putString("clientId", clientId);
+			preferences.flush();
+		}
+		return clientId;
+	}
+
+	private static String generateClientId() {
+		//noinspection SpellCheckingInspection
+		final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		final int numChars = chars.length();
+
+		final StringBuilder sb = new StringBuilder(16);
+		for (int i = 0; i < 16; i++) {
+			final int c = (int) Math.floor(Math.random() * numChars);
+			sb.append(chars.charAt(c));
+		}
+
+		return sb.toString();
 	}
 
 }
