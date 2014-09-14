cfc833241f358171742db8fb60da1d0bf5733d99
wrongcoder <git@wrongcoder.com>
2014-09-14 13:19:09 -0700

    Add developer script to rebuild demo project

---
+++ src/main/scripts/generate-demo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+cd ~/Projects
+if [ -d demo -a -f demo/.gitignore ]; then
+	(cd demo && find . -name .git -prune -o -name '*.iml' -prune -o -type f -print0 | xargs -0 rm)
+fi
+mvn archetype:generate -B -DarchetypeGroupId=com.vdxp -DarchetypeArtifactId=gdx-archetype -DgroupId=com.example.demo -DartifactId=demo -Dversion='1.0.0.BUILD-SNAPSHOT' -DprojectTitle='Demo Project'
+cd demo
+git init
+git add -A .
+git commit --allow-empty -m 'Generated demo project'
+mvn -P desktop,html clean
