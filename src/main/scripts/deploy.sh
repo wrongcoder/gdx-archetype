aaed1e05b3ad5209ff3475dcba2f62a09aac491a
wrongcoder <git@wrongcoder.com>
2015-10-30 22:43:46 -0700

    Make scripts work from anywhere

--- src/main/scripts/deploy.sh
+++ src/main/scripts/deploy.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+SCRIPT=$(python -c 'import os, sys; print os.path.realpath(sys.argv[1])' "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+POM_DIR="$SCRIPT_DIR/../../.."
+[ -d "$SCRIPT_DIR" ] || { echo "Cannot locate this script (looks like it's in $SCRIPT_DIR)"; exit 1; }
+[ -f "$POM_DIR/pom.xml" ] || { echo "Was this script moved? Redefine POM_DIR"; exit 1; }
+cd "$POM_DIR"
+
 mvn clean deploy -DaltDeploymentRepository=local::default::file:../gdx-archetype-mvn
--- src/main/scripts/generate-demo.sh
+++ src/main/scripts/generate-demo.sh
@@ -1,10 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-cd ~/Projects
+
+SCRIPT=$(python -c 'import os, sys; print os.path.realpath(sys.argv[1])' "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+POM_DIR="$SCRIPT_DIR/../../.."
+[ -d "$SCRIPT_DIR" ] || { echo "Cannot locate this script (looks like it's in $SCRIPT_DIR)"; exit 1; }
+[ -f "$POM_DIR/pom.xml" ] || { echo "Was this script moved? Redefine POM_DIR"; exit 1; }
+
+cd "$POM_DIR/.."
+
 if [ -d demo -a -f demo/.gitignore ]; then
-	(cd demo && find . -name .git -prune -o -name '*.iml' -prune -o -type f -print0 | xargs -0 rm)
+	find demo -name .git -prune -o -name '*.iml' -prune -o -type f -print0 | xargs -0 rm
 fi
+
 mvn archetype:generate -B -DarchetypeGroupId=com.vdxp.gdx -DarchetypeArtifactId=gdx-archetype -DgroupId=com.example.demo -DartifactId=demo -Dversion='1.0.0.BUILD-SNAPSHOT' -DprojectTitle='Demo Project'
+
 cd demo
 git init
 git add -A .
